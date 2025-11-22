c7e2b7437bf6936e65b87f12adc424c00b6ba014
Liisi <liisi117@gmail.com>
2025-11-22 21:36:44 +0200

    added DDL and DML to sql

--- sql/sample_query_limited.sql
+++ sql/sample_query_limited.sql
@@ -1,6 +1,6 @@
--- Query data from full and limited access view with limited access user
+-- Query data from the full view and the masked-view by a limited-access user
 SET ROLE analyst_limited;
 SELECT * FROM ukpolice_gold.view_analysis_full LIMIT 5;   -- should not give output
 
 SET ROLE analyst_limited;
-SELECT * FROM ukpolice_gold.view_analysis_limited LIMIT 5;
+SELECT * FROM ukpolice_gold.view_analysis_masked LIMIT 5;
